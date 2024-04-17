2b9428c47a17c56fd63d41cbe3bdf0db553b99e6
PAiN <bibeks337@gmail.com>
2024-04-17 13:24:03 +0545

    download fix

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,16 @@
 import { skills } from "../data";
 
 const Skills = () => {
+  const onButtonClick = () => {
+    const cvUrl = "MIS_report.pdf"; // Change this to the local path of your CV file
+    const link = document.createElement("a");
+    link.href = cvUrl;
+    link.download = 'MIS_report.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    console.log("Download initaited");
+  };
   return (
     <section id="skills">
       <div className="container px-5 py-10 mx-auto">
@@ -34,8 +44,11 @@ const Skills = () => {
             </div>
           ))}
         </div>
+        {/**download cv */}
         <div className="flex justify-center p-20">
-          <button className="border-2 border-black font-poppins font-medium hover:bg-gray-500 hover:text-white ease-in-out 2xl:p-4 xl:p-6 lg:p-4 p-2 shadow-xl ">Download CV</button>
+          <button onClick={onButtonClick} className="border-2 border-black cursor-pointer font-poppins font-medium hover:bg-gray-500 hover:text-white ease-in-out 2xl:p-4 xl:p-6 lg:p-4 p-2 shadow-xl ">
+            Download CV
+          </button>
         </div>
       </div>
     </section>
